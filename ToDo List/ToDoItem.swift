37b74d85584106702bda915b311346c94cf5a1c0
Christopher Ware <warech@bc.edu>
2020-02-11 15:42:59 -0500

    blank line

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Chris Ware on 2/11/20.
+//  Copyright © 2020 Chris Ware. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
